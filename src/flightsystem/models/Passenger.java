0715d67babd557b410cd8e3752eb0a20d9dee9db
Mehak <mehakgarg598@gmail.com>
2025-04-28 22:41:09 +0530

    Added constructor and getter-setter in Notification and Passenger

--- src/flightsystem/models/Passenger.java
+++ src/flightsystem/models/Passenger.java
@@ -1,11 +1,78 @@
 package flightsystem.models;
 
 public class Passenger {
-    private int PassengerId;
-    private String FirstName;
-    private String LastName;
-    private int PhoneNo;
-    private String Email;
-    private String Nationality;
+    private int passengerId;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private int phoneNo;
+    private String email;
+    private String nationality;
 
+    public Passenger(int passengerId, String firstName, String lastName, String dateOfBirth, int phoneNo, String email,
+            String nationality) {
+        this.passengerId = passengerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNo = phoneNo;
+        this.email = email;
+        this.nationality = nationality;
+    }
+
+    public int getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(int passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(int phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
 }

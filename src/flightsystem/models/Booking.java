cce0c50ce356ebf46b2101dfac85451645aff3f8
Mishti <mishti.xd@gmail.com>
2025-04-28 02:37:44 -0700

    Booking attributes added

--- src/flightsystem/models/Booking.java
+++ src/flightsystem/models/Booking.java
@@ -1,5 +1,15 @@
 package flightsystem.models;
 
 public class Booking {
+    private int BookingID;
+    private int FlightID;
+    private int PassengerID;
+    private String BookingDate;
+    private int SeatNumber;
+    private String TravelClass;
+    private double Price;
+    private String PaymentStatus;
+    private String CheckInStatus;
+    private String CheckInTime;
 
 }

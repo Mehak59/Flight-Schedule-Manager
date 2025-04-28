ffb476f732d8ea9315930ddd1885a8245637df0a
HimanshiJolly <himanshijolly3@gmail.com>
2025-04-28 14:50:58 +0530

    adding flight attributes

--- src/flightsystem/models/Flight.java
+++ src/flightsystem/models/Flight.java
@@ -1,5 +1,14 @@
 package flightsystem.models;
 
 public class Flight {
+    private int FlightID;
+    private int FlightNumber;
+    private String Departureairport;
+    private String Arrivalairport;
+    private String DepartureTime;
+    private String ArrivalTime;
+    private int Duration;
+    private String Status;
+    private String Schedules;
 
 }

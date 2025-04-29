5c2c583bbaf52e71dae463e688d0ff4b9875603b
HimanshiJolly <himanshijolly3@gmail.com>
2025-04-29 10:11:48 +0530

    getter setter functions added for flight,baggage.

--- src/flightsystem/models/Baggage.java
+++ src/flightsystem/models/Baggage.java
@@ -1,8 +1,44 @@
 package flightsystem.models;
 
 public class Baggage {
-    private int BaggageID;
-    private int PassengerID;
-    private double Weight;
-    private String Status;
+    private int baggageId;
+    private int passengerId;
+    private double weight;
+    private String status;
+    public Baggage(int baggageId,int passengerId,double weight,String status)
+    {
+        this.baggageId=baggageId;
+        this.passengerId=passengerId;
+        this.weight=weight;
+        this.status=status;
+    }
+    public int getBaggageId(){
+        return baggageId;
+    }
+    public void setBaggageId(int baggageId) {
+        this.baggageId = baggageId;
+    }
+    public int getPassengerId(){
+        return passengerId;
+    }
+    public void setPassengerId(int passengerId)
+    {
+        this.passengerId=passengerId;
+    }
+    public Double getWeight(){
+        return weight;
+    }
+    public void setWeight(double weight)
+    {
+        this.weight=weight;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public void setStatus(String status)
+    {
+        this.status=status;
+    }
+    
 }
+
--- src/flightsystem/models/Flight.java
+++ src/flightsystem/models/Flight.java
@@ -1,14 +1,98 @@
 package flightsystem.models;
 
 public class Flight {
-    private int FlightID;
-    private int FlightNumber;
-    private String Departureairport;
-    private String Arrivalairport;
-    private String DepartureTime;
-    private String ArrivalTime;
-    private int Duration;
-    private String Status;
-    private String Schedules;
+    private int flightId;
+    private int flightNumber;
+    private String departureAirport;
+    private String arrivalAirport;
+    private String departureTime;
+    private String arrivalTime;
+    private int duration; 
+    private String status;
+    private String schedules;
+    public Flight(int flightId, int flightNumber, String departureAirport, String arrivalAirport, 
+                String departureTime, String arrivalTime, int duration, String status, String schedules)
+        {
+        this.flightId = flightId;
+        this.flightNumber = flightNumber;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.duration = duration;
+        this.status = status;
+        this.schedules = schedules;
+    }
 
+    public int getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(int flightId) {
+        this.flightId = flightId;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSchedules() {
+        return schedules;
+    }
+
+    public void setSchedules(String schedules) {
+        this.schedules = schedules;
+    }
 }

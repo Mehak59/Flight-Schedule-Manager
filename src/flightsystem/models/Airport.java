ddc64d43e4eb2db6dbe7a1e250734be9ae8ad60e
Mishti <mishti.xd@gmail.com>
2025-04-28 21:33:44 -0700

    get set added

--- src/flightsystem/models/Airport.java
+++ src/flightsystem/models/Airport.java
@@ -2,7 +2,37 @@ package flightsystem.models;
 
 public class Airport {
     private int airportID;
-    private String airportname;
+    private String airportName;
     private String location;
 
+    public Airport(int airportID, String airportName, String location){
+        this.airportID = airportID;
+        this.airportName = airportName;
+        this.location = location;
+  }   
+   
+  public int getairportID(){
+    return airportID;
+  }
+
+  public void setairportID(int airportID){
+    this.airportID = airportID;
+  }
+
+  public String getairportName(){
+    return airportName;
+  }
+
+  public void setairportName(String airportName){
+    this.airportName = airportName;
+  }
+
+  public String getlocation(){
+    return location;
+  }
+
+  public void setlocation(String location){
+    this.location = location;
+  }
 }
+
--- src/flightsystem/models/Booking.java
+++ src/flightsystem/models/Booking.java
@@ -12,4 +12,96 @@ public class Booking {
     private String checkInStatus;
     private String checkInTime;
 
+    public Booking(int bookingID, int flightID, int passengerID, String bookingDate, int seatNumber, String travelClass, double price, String paymentStatus, String checkInStatus, String checkInTime){
+        this.bookingID = bookingID;
+        this.flightID = flightID;
+        this.passengerID = passengerID;
+        this.bookingDate = bookingDate;
+        this.seatNumber = seatNumber;
+        this.travelClass = travelClass;
+        this.price = price;
+        this.paymentStatus = paymentStatus;
+        this.checkInStatus = checkInStatus;
+        this.checkInTime = checkInTime;
 }
+
+public int getBookingID(){
+    return bookingID;
+}
+
+public void setBookingID(int bookingID){   
+    this.bookingID = bookingID;
+}
+
+public int getFlightID(){
+    return flightID;
+}
+
+public void setFlightID(int flightID){
+    this.flightID = flightID;
+}
+
+public int getPassengerID(){
+    return passengerID;
+}
+
+public void setPassengerID(int passengerID){
+    this.passengerID = passengerID;
+}
+
+public String getBookingDate(){
+    return bookingDate;
+}
+
+public void setBookingDate(String bookingDate){
+    this.bookingDate = bookingDate;
+}
+
+public int getSeatNumber(){
+    return seatNumber;
+}
+
+public void setSeatNumber(int seatNumber){
+    this.seatNumber = seatNumber;
+}
+
+public String getTravelClass(){
+    return travelClass;
+}
+
+public void setTravelClass(String travelClass){
+    this.travelClass = travelClass;
+}
+
+public double getPrice(){
+    return price;
+}
+
+public void setPrice(double price){
+    this.price = price;
+}
+
+public String getPaymentStatus(){
+    return paymentStatus;
+}
+
+public void setPaymentStatus(String paymentStatus){
+    this.paymentStatus = paymentStatus;
+}
+
+public String getCheckInStatus(){
+    return paymentStatus;
+}
+
+public void setCheckInStatus(String checkInStatus){
+    this.checkInStatus = checkInStatus;
+}
+
+public String getCheckInTime(){
+    return checkInTime;
+}
+
+public void setCheckInTime(String checkInTime){
+    this.checkInTime = checkInTime;
+}
+ }

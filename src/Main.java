16d76e5364ca6d440713f1bf64890762d0e4012c
HimanshiJolly <himanshijolly3@gmail.com>
2025-05-02 11:05:09 +0530

    adding exit option and welcome func

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,12 @@ public class Main {
         Register register = new Register(scanner);
         Login login = new Login(scanner);
         boolean flag = true;
+        Welcomemsg();
         while (flag) {
             System.out.println("=== Menu ===");
             System.out.println("1. Register");
             System.out.println("2. Login");
+            System.out.println("3. Exit");
             System.out.print("Enter your choice: ");
 
             String choice = scanner.nextLine();
@@ -24,11 +26,22 @@ public class Main {
                 case "2":
                     login.login();
                     break;
-
+                case "3":
+                    System.out.println("Exiting from the program.Goodbye!");
+                    flag=false;
+                    break;
                 default:
                     System.out.println("Invalid choice. Please enter 1, 2, or 3.");
             }
             System.out.println();
         }
+    }
+    private static void Welcomemsg()
+    {
+      System.out.println("\n**************************");
+      System.out.println("Welcome to the Flight Schedule Manager");
+      System.out.println("Manage your flights, bookings, and check-ins with ease.");
+      System.out.println("Please choose an option: Register, Login, or Exit.");
+      System.out.println("**************************\n");
     }
 }
